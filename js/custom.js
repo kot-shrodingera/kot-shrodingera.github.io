352aa4722576ece673762f558df06eb7567a6b2c
kot-shrodingera <VVe1dmann5He1l>
2017-07-01 23:42:03 +0600

    carousel cnimation functions
    Made unifired function for animation change of all carousels, depending
    on arg passed to callback, and removed console.logs

--- js/custom.js
+++ js/custom.js
@@ -2,27 +2,30 @@
 
   "use strict";
 
-  var owlPrice = $(".price-list.owl-carousel");
-  owlPrice.owlCarousel({items: 1, touchDrag: false, mouseDrag: false, dots: false, animateOut: 'slideOutLeft', animateIn: 'slideInRight'});
-  // owlPrice.owlCarousel({items: 1, touchDrag: false, mouseDrag: false, dots: false});
-  owlPrice.on('translated.owl.carousel', { owlCarousel: owlPrice}, reEnableAnimation);
-
-  var owlDoctors = $(".doctor-photo-container.owl-carousel");
-  owlDoctors.owlCarousel({items: 1, mouseDrag: false});
-
   $(".option-list.owl-carousel").owlCarousel({items: 1, mouseDrag: false});
   $(".news-block.owl-carousel").owlCarousel({items: 1, mouseDrag: false, dots: false, nav: true, navText: []});
   $(".comments-block.owl-carousel").owlCarousel({items: 1, mouseDrag: false});
 
-  $(".price-menu li").click(function() {
+  var owlPrice = $(".price-list.owl-carousel");
+  owlPrice.owlCarousel({items: 1, touchDrag: false, mouseDrag: false, dots: false, animateOut: 'slideOutLeft', animateIn: 'slideInRight'});
+  owlPrice.on('translated.owl.carousel', { owlCarousel: owlPrice}, reEnableAnimation);
+
+  var owlDoctors = $(".doctor-photo-container.owl-carousel");
+  owlDoctors.owlCarousel({items: 1, mouseDrag: false, animateOut: 'slideOutLeft', animateIn: 'slideInRight'});
+  owlDoctors.on('translated.owl.carousel', { owlCarousel: owlDoctors}, reEnableAnimation);
+
+  var priceMenu = $(".price-menu li");
+  var doctorMenu = $(".doctors-list li");
+
+  priceMenu.click(function() {
     $(this).off();
     $(this).siblings().removeClass("active");
     $(this).addClass("active");
-
     owlPrice.trigger('to.owl.carousel', $(this).index());
   });
 
-  $(".doctors-list li").click(function() {
+  doctorMenu.click(function() {
+    $(this).off();
     $(this).siblings().removeClass("active");
     $(this).addClass("active");
     owlDoctors.trigger('to.owl.carousel', $(this).index());
@@ -30,34 +33,37 @@
 
   function reEnableAnimation(event) {
     var owlCarousel = event.data.owlCarousel;
-    console.log($(".price-menu li"));
-    console.log($(owlCarousel.find("li")));
-    console.log(owlCarousel);
-    $(".price-menu li").off();
-
-    $(".price-menu li").click({ owlCarousel: event.data.owlCarousel }, priceTransition);
+    if (owlCarousel == owlPrice) {
+      var menu = priceMenu;
+    }
+    else if (owlCarousel == owlDoctors) {
+      var menu = doctorMenu;
+    }
+    var carouselObj = { owlCarousel: owlCarousel, menu: menu};
+    menu.off();
+    menu.click(carouselObj, transitionDirectionCheck);
   }
 
-  function priceTransition(event) {
-    // console.log(event.data);
-    $(".price-menu li").off();
-    if (($(this).index() < $(this).siblings(".active").index()) && owlPrice.data('owl.carousel').options.animateOut != 'slideOutRight') {
-      owlPrice.data('owl.carousel').options.animateOut = 'slideOutRight';
-      owlPrice.data('owl.carousel').options.animateIn = 'slideInLeft';
-      owlPrice.data('owl.carousel').settings.animateOut = 'slideOutRight';
-      owlPrice.data('owl.carousel').settings.animateIn = 'slideInLeft';
-      owlPrice.trigger('refresh.owl.carousel');
-    } else if (($(this).index() > $(this).siblings(".active").index()) && owlPrice.data('owl.carousel').options.animateOut != 'slideOutLeft') {
-      owlPrice.data('owl.carousel').options.animateOut = 'slideOutLeft';
-      owlPrice.data('owl.carousel').options.animateIn = 'slideInRight';
-      owlPrice.data('owl.carousel').settings.animateOut = 'slideOutLeft';
-      owlPrice.data('owl.carousel').settings.animateIn = 'slideInRight';
-      owlPrice.trigger('refresh.owl.carousel');
+  function transitionDirectionCheck(event) {
+    var owlCarousel = event.data.owlCarousel;
+    var menu = event.data.menu;
+    menu.off();
+    if (($(this).index() < $(this).siblings(".active").index()) && owlCarousel.data('owl.carousel').options.animateOut != 'slideOutRight') {
+      owlCarousel.data('owl.carousel').options.animateOut = 'slideOutRight';
+      owlCarousel.data('owl.carousel').options.animateIn = 'slideInLeft';
+      owlCarousel.data('owl.carousel').settings.animateOut = 'slideOutRight';
+      owlCarousel.data('owl.carousel').settings.animateIn = 'slideInLeft';
+      owlCarousel.trigger('refresh.owl.carousel');
+    } else if (($(this).index() > $(this).siblings(".active").index()) && owlCarousel.data('owl.carousel').options.animateOut != 'slideOutLeft') {
+      owlCarousel.data('owl.carousel').options.animateOut = 'slideOutLeft';
+      owlCarousel.data('owl.carousel').options.animateIn = 'slideInRight';
+      owlCarousel.data('owl.carousel').settings.animateOut = 'slideOutLeft';
+      owlCarousel.data('owl.carousel').settings.animateIn = 'slideInRight';
+      owlCarousel.trigger('refresh.owl.carousel');
     }
     $(this).siblings().removeClass("active");
     $(this).addClass("active");
-
-    owlPrice.trigger('to.owl.carousel', $(this).index());
+    owlCarousel.trigger('to.owl.carousel', $(this).index());
   }
 
 })(jQuery);
