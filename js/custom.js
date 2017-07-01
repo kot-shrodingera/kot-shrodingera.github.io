5beb7c1786725e853078e00ec65fff2fd69bdc71
kot-shrodingera <VVe1dmann5He1l>
2017-07-01 22:46:18 +0600

    Font import, carousel fixes
    - HTTPS font import, removed Raleway font
    - first doctor is set active by default
    - added comments carousel
    - carousel fixes

--- js/custom.js
+++ js/custom.js
@@ -3,16 +3,24 @@
   "use strict";
 
   var owlPrice = $(".price-list.owl-carousel");
-  owlPrice.owlCarousel({items: 1, touchDrag: false, mouseDrag: false, dots: false, animateOut: 'slideOutLeft', animateIn: 'slideInRight', onTranslated: enablePriceMenu});
+  owlPrice.owlCarousel({items: 1, touchDrag: false, mouseDrag: false, dots: false, animateOut: 'slideOutLeft', animateIn: 'slideInRight'});
   // owlPrice.owlCarousel({items: 1, touchDrag: false, mouseDrag: false, dots: false});
+  owlPrice.on('translated.owl.carousel', { owlCarousel: owlPrice}, reEnableAnimation);
 
   var owlDoctors = $(".doctor-photo-container.owl-carousel");
-  owlDoctors.owlCarousel({items: 1, mouseDrag: true});
+  owlDoctors.owlCarousel({items: 1, mouseDrag: false});
 
   $(".option-list.owl-carousel").owlCarousel({items: 1, mouseDrag: false});
   $(".news-block.owl-carousel").owlCarousel({items: 1, mouseDrag: false, dots: false, nav: true, navText: []});
+  $(".comments-block.owl-carousel").owlCarousel({items: 1, mouseDrag: false});
 
-  enablePriceMenu();
+  $(".price-menu li").click(function() {
+    $(this).off();
+    $(this).siblings().removeClass("active");
+    $(this).addClass("active");
+
+    owlPrice.trigger('to.owl.carousel', $(this).index());
+  });
 
   $(".doctors-list li").click(function() {
     $(this).siblings().removeClass("active");
@@ -20,13 +28,18 @@
     owlDoctors.trigger('to.owl.carousel', $(this).index());
   })
 
-  function enablePriceMenu() {
+  function reEnableAnimation(event) {
+    var owlCarousel = event.data.owlCarousel;
+    console.log($(".price-menu li"));
+    console.log($(owlCarousel.find("li")));
+    console.log(owlCarousel);
     $(".price-menu li").off();
-    $(".price-menu li").click(priceTransition);
-    console.log("enablePriceMenu");
+
+    $(".price-menu li").click({ owlCarousel: event.data.owlCarousel }, priceTransition);
   }
 
-  function priceTransition() {
+  function priceTransition(event) {
+    // console.log(event.data);
     $(".price-menu li").off();
     if (($(this).index() < $(this).siblings(".active").index()) && owlPrice.data('owl.carousel').options.animateOut != 'slideOutRight') {
       owlPrice.data('owl.carousel').options.animateOut = 'slideOutRight';
@@ -34,14 +47,12 @@
       owlPrice.data('owl.carousel').settings.animateOut = 'slideOutRight';
       owlPrice.data('owl.carousel').settings.animateIn = 'slideInLeft';
       owlPrice.trigger('refresh.owl.carousel');
-      console.log("change to left");
     } else if (($(this).index() > $(this).siblings(".active").index()) && owlPrice.data('owl.carousel').options.animateOut != 'slideOutLeft') {
       owlPrice.data('owl.carousel').options.animateOut = 'slideOutLeft';
       owlPrice.data('owl.carousel').options.animateIn = 'slideInRight';
       owlPrice.data('owl.carousel').settings.animateOut = 'slideOutLeft';
       owlPrice.data('owl.carousel').settings.animateIn = 'slideInRight';
       owlPrice.trigger('refresh.owl.carousel');
-      console.log("change to right");
     }
     $(this).siblings().removeClass("active");
     $(this).addClass("active");
