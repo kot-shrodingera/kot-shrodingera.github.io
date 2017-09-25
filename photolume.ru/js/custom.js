6932ba7559a71b2c9ec010b257140711e608d0a1
kot-shrodingera <VVe1dmann5He1l>
2017-09-25 22:54:13 +0600

    Sticky menu

--- photolume.ru/js/custom.js
+++ photolume.ru/js/custom.js
@@ -1,7 +1,15 @@
-// (function ($) {
+(function ($) {
 
-//   "use strict";
+  "use strict";
 
+$(window).scroll(function(){
+  var docscroll = $(document).scrollTop();
+  console.log(docscroll);
+  if (docscroll > 60) {
+    $('.sticky-menu').removeClass('hidden');
+  } else {
+    $('.sticky-menu').addClass('hidden');
+  }
+});
 
-
-// })(jQuery);
+})(jQuery);
