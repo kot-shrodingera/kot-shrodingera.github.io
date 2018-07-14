65078cdf907e447ce58d1efdc63b92ebe9d5e009
kot-shrodingera <kot.shrodingera@gmail.com>
2018-07-14 21:16:49 +0600

    450px + grid gallery

--- motohranenie.org/js/custom.js
+++ motohranenie.org/js/custom.js
@@ -2,9 +2,7 @@
 
   "use strict";
 
-  $('input[name^="date"]').datepicker({
-    
-  });
+  $('input[name^="date"]').datepicker({});
 
   $('nav').localScroll();
   $('header .intro').localScroll();
@@ -14,6 +12,33 @@
   })
 
   $('.pao .order input[name="phone"]').mask('+7 (999) 999-99-99');
-  // $('.pao .order input[name^="date"]').mask('99.99.9999');
+
+  if (matchMedia) {
+    const mq = window.matchMedia("(max-width: 765px)");
+    mq.addListener(WidthChange);
+    WidthChange(mq);
+  }
+
+  function WidthChange(mq) {
+    if (mq.matches) {
+      $('.gallery .container').removeClass('grid');
+      $('.gallery .container').addClass('owl-carousel owl-theme');
+      $('.owl-carousel').owlCarousel({
+        loop: true,
+        nav: false,
+        dots: false,
+        center: true,
+        margin: 20,
+        autoWidth: true,
+        startPosition: 2,
+        items: 2
+      });
+    } else {
+      $('.owl-carousel').owlCarousel('destroy');
+      $('.gallery .container').removeClass('owl-carousel owl-theme');
+      $('.gallery .container').addClass('grid');
+    }
+
+  }
 
 })(jQuery);
